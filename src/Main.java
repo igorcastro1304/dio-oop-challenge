64594d19a3764530a579515f9b86109156cb7fde
igorcastro1304 <igor.hoffmann@al.infnet.edu.br>
2025-08-02 16:16:39 -0400

    chore: Testando os objetos criados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.time.LocalDateTime;
+
+import br.com.dio.challenge.domain.Course;
+import br.com.dio.challenge.domain.Mentoring;
+
 public class Main {
-
 	public static void main(String[] args) {
+		Mentoring mentoring01 = new Mentoring();
+		mentoring01.setName("Como conseguir sua vaga na GFT?");
+		mentoring01.setDescription("Mentoria focada em desenvolver o aluno para a vaga de estágio da DIO.");
+		mentoring01.setDate(LocalDateTime.of(2025, 4, 13, 15, 30));
 		
-
+		Course course01 = new Course();
+		course01.setName("Curso Java");
+		course01.setDescription("Curso focado em desenvolvimento Java.");
+		course01.setWorkload(80);
+		
+		System.out.println(mentoring01);
+		System.out.println(course01);
 	}
-
 }
